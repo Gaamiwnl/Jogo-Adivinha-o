70cc47e443b0977ba28b7803cd0697375e308b47
Felipe <gaamngt@gmail.com>
2025-08-07 15:22:50 -0300

    criando variavél

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "**********************" << endl;  
+    cout << "* Jogo da Adivinhacao *" << endl;
+    cout << "**********************" << endl;
+    
+    int numero_secreto = 42;
+    cout << "o numero secreto e " <<  numero_secreto << " nao conte para ninguem" << endl;
+
+
+}
